dd18828c5bc1018a94a4979343b8ca0e4218753a
Alejandro Ohrnialian Bengolea <104745560+AOB1992@users.noreply.github.com>
2024-03-01 00:50:53 -0300

    Update Auth0Provider.js

--- src/Auth0Provider/Auth0Provider.js
+++ src/Auth0Provider/Auth0Provider.js
@@ -3,15 +3,15 @@ import { useNavigate } from "react-router-dom"
 import { Auth0Provider } from "@auth0/auth0-react"
 
 
- 
+
 const Auth0ProviderWithHistory = ({ children }) => {
-let REACT_APP_GAMING_PALACE_DOMAIN = dev-a22su4rxz5izta6g.us.auth0.com
-let REACT_APP_GAMING_PALACE_CLIENT_ID = hvd6p2gYhOovkoZ3juZzaoeZl9npr1Me
+//let REACT_APP_GAMING_PALACE_DOMAIN = dev-a22su4rxz5izta6g.us.auth0.com
+//let REACT_APP_GAMING_PALACE_CLIENT_ID = hvd6p2gYhOovkoZ3juZzaoeZl9npr1Me
  // original const domain = process.env.REACT_APP_GAMING_PALACE_DOMAIN
  // const clientId = process.env.REACT_APP_GAMING_PALACE_CLIENT_ID
 
-   const domain = REACT_APP_GAMING_PALACE_DOMAIN
-  const clientId = REACT_APP_GAMING_PALACE_CLIENT_ID
+   const domain = dev-a22su4rxz5izta6g.us.auth0.com
+  const clientId = hvd6p2gYhOovkoZ3juZzaoeZl9npr1Me
   
   const history = useNavigate();
 
